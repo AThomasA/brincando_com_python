d6d82c56a1ab216216e0d1dde796d33fb206a761
Ath0maz <allan.thomas.a.o@gmail.com>
2025-10-17 10:40:24 -0300

    intro: Brincadeira 2 - concluida + ajuste brincadeira 1

--- intro_exercicios/ex_01.py
+++ intro_exercicios/ex_01.py
@@ -1,3 +1,12 @@
+#Brincadeira 1 - Variaveis
 #Definindo variaveis com dados do jogador
 # %%
 nome_do_jogador = input("Insira seu nome: ")
+idade_do_jogador = input("Insira sua idade: ")
+job_do_jogador = input("Insira sua profissao: ")
+
+print("Seu nome é : ", nome_do_jogador, 
+      "você tem ", idade_do_jogador, 
+      "e sua profissão nessa aventura é ", job_do_jogador, 
+      ".", 
+      "Bem vindo aventureiro! Enjoy the journey!")
---
+++ intro_exercicios/ex_02.py
@@ -0,0 +1,10 @@
+#Brincadeira 2 - Comentarios
+# Comentarios de uma ou mais linhas usando #(comentarios em linha) e ''' '''(multiplas linhas)
+
+#Aventureiros da Codificação
+
+'''Aqui a nossa aventura começa, vamos explorar o mundo pythonico de uma forma leve e divertida.
+Todos que acompanharem nossa aventura vao conseguir conhecer o poder do python e como ele pode ser util
+nas nossas vidas, seja como desenvolvedor, como simpatizante da tecnologia ou apenas um curioso.
+Vou usar exemplos baseados em jogos e no dia a dia tambem para facilitar a compreensao
+'''
